c28f6c5a43a6a744bc7c045ab529a6fd1c2d6099
Yann de Thé <yann@johncloud.fr>
2020-05-13 22:02:44 +0200

    Test complet

--- PhotoManagement/Image.py
+++ PhotoManagement/Image.py
@@ -1,8 +1,26 @@
+import datetime
+import os
+import time
+
 from PIL import Image
+from PIL.ExifTags import TAGS
 
 
 def read_capture_date_and_time(pth):
     im = Image.open(pth)
     exif_data = im._getexif()
-    return exif_data
+    im.close()
     
+    labeled = {}
+    for (key, val) in exif_data.items():
+        labeled[TAGS.get(key)] = val
+
+    # '2020:04:02 12:58:12'
+    date_time_str = labeled['DateTimeOriginal']
+    date_time_obj = datetime.datetime.strptime(date_time_str, '%Y:%m:%d %H:%M:%S')
+
+    return date_time_obj
+
+def modTime(pth, dt):
+    modTime = time.mktime(dt.timetuple())
+    os.utime(pth, (modTime, modTime))
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,15 +1,26 @@
 import os
 import unittest
+import datetime
+from shutil import copyfile
 
-from PhotoManagement.Image import read_capture_date_and_time
+from PhotoManagement.Image import read_capture_date_and_time, modTime
 
 
 class TestImage (unittest.TestCase):
+    def setUp(self):
+        pth1 = os.path.join(os.getcwd(),"tests","children.jpg")
+        pth2 = os.path.join(os.getcwd(),"tests","test.jpg")
+        copyfile(pth1, pth2)
+
     def test_exif(self):
-        pth = os.path.join(os.getcwd(),"tests","fleur.jpg")
-        exif_data = read_capture_date_and_time(pth)
-        dat = str(exif_data)
-        f = open('log.txt','w')
-        f.write(dat)
-        f.close()
+        pth = os.path.join(os.getcwd(),"tests","test.jpg")
+        date = read_capture_date_and_time(pth)
+
+        self.assertEqual(date, datetime.datetime(2020, 4, 2, 12, 58, 12))
+        
+        date2 = datetime.datetime(2021, 4, 2, 12, 58, 12)
+        modTime(pth, date2)
+        date = read_capture_date_and_time(pth)
+        self.assertEqual(date, date2)
+
         
