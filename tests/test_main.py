bafc4f7808303b391b440f45ba77fdca846c75f0
Yann de The <ydethe@gmail.com>
2020-05-14 08:18:09 +0000

    Ajout lecture donnees png

--- tests/test_main.py
+++ tests/test_main.py
@@ -11,18 +11,35 @@ class TestImage (unittest.TestCase):
         pth1 = os.path.join(os.getcwd(),"tests","children.jpg")
         pth2 = os.path.join(os.getcwd(),"tests","test.jpg")
         copyfile(pth1, pth2)
+        
+        pth1 = os.path.join(os.getcwd(),"tests","mountain.png")
+        pth2 = os.path.join(os.getcwd(),"tests","test.png")
+        copyfile(pth1, pth2)
 
-    def test_exif(self):
+    def test_exif_jpeg(self):
         pth = os.path.join(os.getcwd(),"tests","test.jpg")
         date = read_capture_date_and_time(pth)
 
         self.assertEqual(date, datetime.datetime(2020, 4, 2, 12, 58, 23))
         
-    def test_mod_time(self):
+    def test_mod_time_jpeg(self):
         pth = os.path.join(os.getcwd(),"tests","test.jpg")
         date2 = datetime.datetime(2021, 4, 2, 12, 58, 12)
         setModTime(pth, date2)
         date = getModTime(pth)
         self.assertEqual(date, date2)
+
+    def test_exif_png(self):
+        pth = os.path.join(os.getcwd(),"tests","test.png")
+        date = read_capture_date_and_time(pth)
+
+        self.assertEqual(date, datetime.datetime(2020, 4, 2, 12, 58, 23))
+        
+    # def test_mod_time_png(self):
+        # pth = os.path.join(os.getcwd(),"tests","test.png")
+        # date2 = datetime.datetime(2021, 4, 2, 12, 58, 12)
+        # setModTime(pth, date2)
+        # date = getModTime(pth)
+        # self.assertEqual(date, date2)
 
         
