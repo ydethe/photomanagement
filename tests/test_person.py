55bbde63042ff7e06277a1fa2f5be3e2ed6caf1c
Yann de Thé <ydethe@gmail.com>
2021-06-11 21:05:08 +0200

    Import personne OK avec phonenumbers et date de naissance

--- tests/test_person.py
+++ tests/test_person.py
@@ -1,7 +1,8 @@
 import os
 import yaml
-
+from datetime import datetime
 from mongoengine import connect
+import phonenumbers
 
 from PhotoManagement import logger
 from PhotoManagement.db import Face, Photo, Person
@@ -12,6 +13,28 @@ from PhotoManagement.db import Face, Photo, Person
 connect("photo_mgt")
 
 
+def fmt_tel(s):
+    try:
+        if not s.startswith("+") and not s.startswith("00"):
+            z = phonenumbers.parse(s, "FR")
+        elif s.startswith("00"):
+            z = phonenumbers.parse("+" + s[2:])
+        elif s.startswith("+"):
+            z = phonenumbers.parse(s)
+        else:
+            pass
+
+        if not phonenumbers.is_valid_number(z):
+            return ""
+
+        s = phonenumbers.format_number(z, phonenumbers.PhoneNumberFormat.INTERNATIONAL)
+
+        return s
+    except Exception as e:
+        logger.error(e)
+        return ""
+
+
 def clearLinkPersonFace():
     for face in Face.objects():
         face.person = None
@@ -20,13 +43,10 @@ def clearLinkPersonFace():
     Person.objects().delete()
 
 
-# clearLinkPersonFace()
-# exit(0)
-
 # Face.exportAll()
 # exit(0)
 
-# clearLinkPersonFace()
+clearLinkPersonFace()
 root0 = "faces"
 
 for root, dirs, files in os.walk(root0):
@@ -36,6 +56,10 @@ for root, dirs, files in os.walk(root0):
 
     with open(os.path.join(root, "info.yml"), "r") as f:
         info = yaml.load(f, Loader=yaml.FullLoader)
+    info["mobile_perso"] = fmt_tel(info["mobile_perso"])
+    info["mobile_pro"] = fmt_tel(info["mobile_pro"])
+    info["fix_pro"] = fmt_tel(info["fix_pro"])
+    info["date_birth"] = datetime.strptime(info["date_birth"], "%d/%m/%Y")
 
     pers = Person(**info)
     pers.save()
