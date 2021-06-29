3db119e814b3fe73321ddc2dcab916d618b23751
Yann de Thé <ydethe@gmail.com>
2021-06-29 09:08:02 +0200

    Recognition web OK

--- PhotoManagement/blueprints/photo/routes.py
+++ PhotoManagement/blueprints/photo/routes.py
@@ -23,16 +23,11 @@ from ...Face import Face
 
 @photo_bp.route("/", methods=["POST", "GET"])
 def photo():
-    t0 = time.time()
-
-    am = AirtableManager()
-
     personslist = ["Inconnu"]
     pidList = [""]
     for pers in Person.objects():
         personslist.append(pers.complete_name)
         pidList.append(str(pers.id))
-    logger.debug("%.3f" % (time.time() - t0))
 
     if request.method == "POST":
         # ImmutableMultiDict([('photo_id', '60d4db7bd9685a94e51b3d25'), ('input-yann-blaudin-de-the', 'alix-de-chanterac'), ('input-ines-blaudin-de-the', 'ines-blaudin-de-the')])
@@ -43,24 +38,34 @@ def photo():
             v = request.form[k]
             if k == "photo_id":
                 photo = Photo.objects(id=v).first()
-                nphoto = photo.next()
+                nphoto = Photo.objects(date_taken__gt=photo.date_taken).first()
                 id = nphoto.id
                 continue
 
             face_id = k[6:]
-            ipers = pidList.index(v)
-            pers_id = pidList[ipers]
-            logger.debug("%s, %s" % (face_id, pers_id))
             qf = Face.objects(id=face_id)
-            qp = Person.objects(id=pers_id)
-            if qp.count() != 1:
-                logger.error("Person id unknown %s" % pers_id)
             if qf.count() != 1:
                 logger.error("Face id unknown %s" % face_id)
-            if qp.count() == 1 and qf.count() == 1:
-                pers = qp.first()
+                face = None
+            else:
                 face = qf.first()
-                face.affectToPersonAndSaveAll(pers)
+
+            if v == "supp" and not face is None:
+                face = qf.first()
+                face.delete()
+            else:
+                ipers = pidList.index(v)
+                pers_id = pidList[ipers]
+                qp = Person.objects(id=pers_id)
+                if qp.count() != 1:
+                    logger.error("Person id unknown %s" % pers_id)
+                    pers = None
+                else:
+                    pers = qp.first()
+                    logger.info(
+                        "Face id %s affected to %s" % (face_id, pers.complete_name)
+                    )
+                    face.affectToPersonAndSaveAll(pers)
 
     else:
         id = request.args.get("id", None)
@@ -71,7 +76,6 @@ def photo():
     photo = Photo.objects(id=id).first()
     b64 = base64.b64encode(photo.photo.read())
     b64_photo = b64.decode("UTF-8")
-    logger.debug("%.3f" % (time.time() - t0))
 
     b64_faces = []
     names_slug = []
@@ -86,13 +90,18 @@ def photo():
             else:
                 pid = person.id
 
-        names_slug.append((pid, face.id))
+        names_slug.append(
+            (
+                pid,
+                face.id,
+                not face.recognition_score is None and not face.manually_tagged,
+            )
+        )
         img = face.getImage()
         buf = io.BytesIO()
         img.save(buf, format="JPEG")
         b64 = base64.b64encode(buf.getbuffer())
         b64_faces.append(b64.decode("UTF-8"))
-    logger.debug("%.3f" % (time.time() - t0))
 
     return render_template(
         "photo.html",
